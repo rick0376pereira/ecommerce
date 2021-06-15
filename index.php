bb7fcd59a8ee3b488076f992ab82e0ca24d05555
rick0376pereira <lhpsystems0376@bol.com.br>
2021-06-15 19:50:54 -0300

    criamos  pageadmin e tpl admin

--- index.php
+++ index.php
@@ -1,20 +1,20 @@
 <?php 
-
+session_start();
 require_once("vendor/autoload.php");
 
-$app = new \Slim\Slim();
+use \Slim\Slim;
+
+$app = new Slim();
 
 $app->config('debug', true);
 
-$app->get('/', function() {
-    
-	$sql = new Hcode\DB\Sql();
-
-	$results = $sql->select("SELECT * FROM tb_users");
-
-	echo json_encode($results);
-	
-});
+require_once("functions.php");
+require_once("site.php");
+require_once("admin.php");
+require_once("admin-users.php");
+require_once("admin-categories.php");
+require_once("admin-products.php");
+require_once("admin-orders.php");
 
 $app->run();
 
